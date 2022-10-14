bd9f5b3804f78effd0121eb599eff8cfa25eb285
dopesolos20 <dopesolos20@gmail.com>
2022-10-14 07:49:17 -0700

    C is fun

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,23 +1,13 @@
 #include <stdarg.h>
-
 #include <stdio.h>
-
 #include "variadic_functions.h"
 
-
-
 /**
-
  * print_numbers - prints numbers, followed by a new line.
-
  * @separator: separator to print between numbers
-
  * @n: number of numbers to print
-
  *
-
  * Return: void
-
  */
 
 void print_numbers(const char *separator, const unsigned int n, ...)
